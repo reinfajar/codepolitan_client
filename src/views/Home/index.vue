a4b2ac361de3a9a31c6cb5375f752b49cb6209e2
reinfajar <tsanimfajar@outlook.com>
2020-07-24 15:44:04 +0700

    add a bit

---
+++ src/views/Home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <section class="text-gray-700 body-font relative">
+      <div class="container px-5 py-24 mx-64">
+        <div class="flex flex-col text-center w-full mb-12">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Login</h1>
+        </div>
+        Facebook
+        <br />
+        Google
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
